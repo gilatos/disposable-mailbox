345c2c8315d23cac2e8eeb6364dec5dc04ad4765
Gilatos <44944769+gilatos@users.noreply.github.com>
2019-03-19 22:52:41 +0800

    ico

--- inbox/frontend.template.php
+++ inbox/frontend.template.php
@@ -65,7 +65,10 @@ function printMessageBody($email, $purifier) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+	<link rel="shortcut icon" href="favicon.ico">
+    <meta name="robots" content="noindex" />
+	<meta name="referrer" content="no-referrer">
+	
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="assets/bootstrap/4.1.1/bootstrap.min.css"
           integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
